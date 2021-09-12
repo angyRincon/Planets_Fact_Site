35c212e9518610e189d255a9c974dc22a481ca5c
sam y lau <tazedwin@gmail.com>
2021-09-11 20:12:43 -0500

    Finished navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,8 +1,45 @@
 import planets from '../Planets.json'
+import {useEffect} from "react";
 
 export const NavBar = () => {
+
+    const handleShowNav = () => {
+        let menuList = document.getElementById("menuList")
+        if (menuList.style.maxHeight === "0px") {
+            menuList.style.maxHeight = "400px"
+        } else {
+            menuList.style.maxHeight = "0px"
+        }
+    }
+
+    useEffect(() => {
+        let menuList = document.getElementById("menuList")
+        menuList.style.maxHeight = "400px"
+    }, [])
+
     return (
         <div className="navbar">
+
+            <a href="#" className="logo">The planets</a>
+
+            <nav>
+                <ul id="menuList">
+                    {planets.map(planet => (
+                        <li key={planet.name}>
+                            <a href="#">
+                                {planet.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+            <a href="#" className="menu-icon" onClick={handleShowNav}>
+                <i className="fas fa-bars"/>
+            </a>
+
+        </div>
+        /*<div className="navbar">
 
             <a href="#" className="logo">The Planets</a>
 
@@ -20,6 +57,6 @@ export const NavBar = () => {
                 <li className="fas fa-bars"/>
             </a>
 
-        </div>
+        </div>*/
     )
 }
