b5890aed56974b0e24dcf693d1a06ea4db45d71a
sam y lau <tazedwin@gmail.com>
2021-09-13 19:17:00 -0500

    Fixed responsive design without boxes details

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -2,30 +2,41 @@ import {Details} from "./Details";
 
 export const Container = () => {
     return (
-        <div>
-            <div className="container">
-                <img src="/images/planets/earth.png" alt="earth"/>
+        <>
+            <div>
+                <div className="buttons-mobile" id="buttons">
+                    <a href="#">Overview</a>
+                    <a href="#">Structure</a>
+                    <a href="#">Geology</a>
+                </div>
+                <div className="container">
 
-                <div className="content">
-                    <div className="description">
-                        <h1>Earth</h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolor dolorum eaque eum
-                            facere fuga fugiat illum inventore ipsam iste natus obcaecati porro quae repellat saepe
-                            temporibus, veritatis. Alias aliquid amet blanditiis corporis dolor, dolorum eius enim fuga
+                    <img className="mover-img" src="/images/planets/earth.png" alt="earth"/>
 
-                        </p>
-                        <a href="#">Source: <span>Wikipedia</span><i className="fas fa-share-square"/></a>
-                    </div>
+                    <div className="content">
+                        <div className="description">
+                            <h1>Earth</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolor dolorum eaque
+                                eum
+                                facere fuga fugiat illum inventore ipsam iste natus obcaecati porro quae repellat saepe
+                                temporibus, veritatis. Alias aliquid amet blanditiis corporis dolor, dolorum eius enim
+                                fuga
+                            </p>
+                            <a href="#">Source: <span>Wikipedia</span><i className="fas fa-share-square"/></a>
+                        </div>
 
-                    <div className="buttons" id="buttons">
-                        <a href="#">01 <span>Overview</span></a>
-                        <a href="#">02 <span>Internal Structure</span></a>
-                        <a href="#">03 <span>Surface Geology</span></a>
+                        <div className="buttons" id="buttons">
+                            <a href="#">01 <span>Overview</span></a>
+                            <a href="#">02 <span>Internal Structure</span></a>
+                            <a href="#">03 <span>Surface Geology</span></a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            {/*<Details/>*/}
+        </>
     )
 }
 // ab76e5e2-492d-4287-b211-0475c868dc80
--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,6 +1,27 @@
 export const Details = () => {
     return (
-       <>
-       </>
+        <div>
+            <div className="details-container">
+                <div className="detail-item col-1">
+                    <h6>Rotation Time</h6>
+                    <h4>0.99 days</h4>
+                </div>
+
+                <div className="detail-item col-2">
+                    <h6>Revolution time</h6>
+                    <h4>365.26 days</h4>
+                </div>
+
+                <div className="detail-item col-3">
+                    <h6>Radius</h6>
+                    <h4>6,371 km</h4>
+                </div>
+
+                <div className="detail-item col-4">
+                    <h6>Average Temp</h6>
+                    <h4>16°C</h4>
+                </div>
+            </div>
+        </div>
     )
 }
